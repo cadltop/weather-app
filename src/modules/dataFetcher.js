296cde7ad4610fc34d3a4ae78b3609594fcb3276
cadltop <caesarfromthecross.808@gmail.com>
2024-09-15 17:10:10 -0500

    Optimize module

--- src/modules/dataFetcher.js
+++ src/modules/dataFetcher.js
@@ -1,18 +1,16 @@
-export const dataFetcher = {
-  async setLocation(text) {
-    const response = await fetch(
-      "https://weather.visualcrossing.com/VisualCrossingWebServices" +
-        `/rest/services/timeline/${text}?key=FX2MGM34YSYBXLSDHZ6QUPTU4`,
-      { mode: "cors" },
-    );
-    const weather = response.json().then(function (responseJSON) {
-      const address = responseJSON.resolvedAddress;
-      const timezone = responseJSON.timezone;
-      const conditions = responseJSON.currentConditions.conditions;
-      const time = responseJSON.currentConditions.datetime;
-      const temperature = responseJSON.currentConditions.temp;
-      return { address, timezone, conditions, time, temperature };
-    });
-    return weather;
-  },
-};
+export default async function (text) {
+  const response = await fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices" +
+      `/rest/services/timeline/${text}?key=FX2MGM34YSYBXLSDHZ6QUPTU4`,
+    { mode: "cors" },
+  );
+  const weather = response.json().then(function (responseJSON) {
+    const address = responseJSON.resolvedAddress;
+    const timezone = responseJSON.timezone;
+    const conditions = responseJSON.currentConditions.conditions;
+    const time = responseJSON.currentConditions.datetime;
+    const temperature = responseJSON.currentConditions.temp;
+    return { address, timezone, conditions, time, temperature };
+  });
+  return weather;
+}
