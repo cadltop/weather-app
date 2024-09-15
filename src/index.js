29f638e0aa4904f4e485a856b40b04af777a0e63
cadltop <caesarfromthecross.808@gmail.com>
2024-09-15 00:08:33 -0500

    Set getWeather to log the response of API call

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import "normalize.css";
 import "./index.css";
+
+function getWeather(location) {
+  const newLocation = location.toLowerCase();
+  const APIKey = "FX2MGM34YSYBXLSDHZ6QUPTU4";
+
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${newLocation}?key=${APIKey} `,
+  )
+    .then(function (response) {
+      return console.log(response);
+    })
+    .catch(function (error) {
+      return console.error(error);
+    });
+}
