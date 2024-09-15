36e3e939cb646b9849d93c6ba70820ee88baabd0
cadltop <caesarfromthecross.808@gmail.com>
2024-09-15 16:53:12 -0500

    Refactor callback to async function

--- src/modules/dataFetcher.js
+++ src/modules/dataFetcher.js
@@ -1,26 +1,18 @@
-export default {
-  set location(text) {
-    fetch(
+export const dataFetcher = {
+  async setLocation(text) {
+    const response = await fetch(
       "https://weather.visualcrossing.com/VisualCrossingWebServices" +
         `/rest/services/timeline/${text}?key=FX2MGM34YSYBXLSDHZ6QUPTU4`,
       { mode: "cors" },
-    )
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (responseJSON) {
-        const address = responseJSON.resolvedAddress;
-        const timezone = responseJSON.timezone;
-        const conditions = responseJSON.currentConditions.conditions;
-        const time = responseJSON.currentConditions.datetime;
-        const temperature = responseJSON.currentConditions.temp;
-        return { address, timezone, conditions, time, temperature };
-      })
-      .then(function (data) {
-        console.log(data);
-      })
-      .catch(function (error) {
-        return error;
-      });
+    );
+    const weather = response.json().then(function (responseJSON) {
+      const address = responseJSON.resolvedAddress;
+      const timezone = responseJSON.timezone;
+      const conditions = responseJSON.currentConditions.conditions;
+      const time = responseJSON.currentConditions.datetime;
+      const temperature = responseJSON.currentConditions.temp;
+      return { address, timezone, conditions, time, temperature };
+    });
+    return weather;
   },
 };
