bb654e804a4fa8a6f4c74a411be9d2e3b1e9beca
cadltop <caesarfromthecross.808@gmail.com>
2024-09-15 16:54:36 -0500

    Select table data cells and fill with data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import "normalize.css";
 import "./index.css";
 
-import dataFetcher from "./modules/dataFetcher";
+import { dataFetcher } from "./modules/dataFetcher";
 
 const formInputs = {
   location: document.querySelector("input#location"),
   search: document.querySelector('input[type="submit"]'),
 };
 
+const tableDataCells = {
+  address: document.querySelector("td.address"),
+  timezone: document.querySelector("td.timezone"),
+  conditions: document.querySelector("td.conditions"),
+  time: document.querySelector("td.time"),
+  temperature: document.querySelector("td.temperature"),
+};
+
 formInputs.search.addEventListener("click", (event) => {
-  dataFetcher.location = formInputs.location.value;
+  dataFetcher.setLocation(formInputs.location.value).then(function (weather) {
+    tableDataCells.address.innerHTML = weather.address;
+    tableDataCells.timezone.innerHTML = weather.timezone;
+    tableDataCells.conditions.innerHTML = weather.conditions;
+    tableDataCells.time.innerHTML = weather.time;
+    tableDataCells.temperature.innerHTML = weather.temperature;
+  });
   event.preventDefault();
 });
