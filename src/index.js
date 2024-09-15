e88fe4bd0f24734f1d4440bcd7aa8ee5e7a9b376
cadltop <caesarfromthecross.808@gmail.com>
2024-09-15 12:31:26 -0500

    Return relevant data from response.

--- src/index.js
+++ src/index.js
@@ -7,11 +7,20 @@ function getWeather(location) {
 
   fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${newLocation}?key=${APIKey} `,
+    { mode: "cors" },
   )
     .then(function (response) {
-      return console.log(response);
+      return response.json();
+    })
+    .then(function (responseJSON) {
+      const address = responseJSON.resolvedAddress;
+      const timezone = responseJSON.timezone;
+      const conditions = responseJSON.currentConditions.conditions;
+      const time = responseJSON.currentConditions.datetime;
+      const temperature = responseJSON.currentConditions.temp;
+      return { address, timezone, conditions, time, temperature };
     })
     .catch(function (error) {
-      return console.error(error);
+      return error;
     });
 }
