d66ed0a224ada79debbfe88271f9497b1c99c402
cadltop <caesarfromthecross.808@gmail.com>
2024-09-15 17:11:58 -0500

    Readjust references

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import "normalize.css";
 import "./index.css";
 
-import { dataFetcher } from "./modules/dataFetcher";
-
-const formInputs = {
-  location: document.querySelector("input#location"),
-  search: document.querySelector('input[type="submit"]'),
-};
-
-const tableDataCells = {
-  address: document.querySelector("td.address"),
-  timezone: document.querySelector("td.timezone"),
-  conditions: document.querySelector("td.conditions"),
-  time: document.querySelector("td.time"),
-  temperature: document.querySelector("td.temperature"),
-};
+import dataFetcher from "./modules/dataFetcher";
+import { formInputs, tableDataCells } from "./modules/pageElements";
 
 formInputs.search.addEventListener("click", (event) => {
-  dataFetcher.setLocation(formInputs.location.value).then(function (weather) {
+  dataFetcher(formInputs.location.value).then(function (weather) {
     tableDataCells.address.innerHTML = weather.address;
     tableDataCells.timezone.innerHTML = weather.timezone;
     tableDataCells.conditions.innerHTML = weather.conditions;
