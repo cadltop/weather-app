225e9a401b060fd44ef79383d695e4ee5a2695b7
cadltop <caesarfromthecross.808@gmail.com>
2024-09-15 20:36:43 -0500

    Show loading component

--- src/index.js
+++ src/index.js
@@ -5,14 +5,22 @@ import dataFetcher from "./modules/dataFetcher";
 import { formInputs, tableDataCells } from "./modules/pageElements";
 
 formInputs.search.addEventListener("click", (event) => {
-  dataFetcher(formInputs.location.value).then(function (weather) {
-    tableDataCells.address.innerHTML = weather.address;
-    tableDataCells.timezone.innerHTML = weather.timezone.replace("_", " ");
-    tableDataCells.conditions.innerHTML = weather.conditions;
-    tableDataCells.time.innerHTML = weather.time;
-    tableDataCells.temperature.innerHTML = `${weather.temperature} °F`;
-
-    document.querySelector("table").style.display = "table";
-  });
+  if (formInputs.location.value !== "") {
+    const p = document.createElement('p');
+    p.innerHTML = "Loading...";
+    const container = document.querySelector('div#container');
+    container.append(p);
+    
+    dataFetcher(formInputs.location.value).then(function (weather) {
+      tableDataCells.address.innerHTML = weather.address;
+      tableDataCells.timezone.innerHTML = weather.timezone.replace("_", " ");
+      tableDataCells.conditions.innerHTML = weather.conditions;
+      tableDataCells.time.innerHTML = weather.time;
+      tableDataCells.temperature.innerHTML = `${weather.temperature} °F`;
+      
+      container.removeChild(p);
+      document.querySelector("table").style.display = "table";
+    });
+  }
   event.preventDefault();
 });
