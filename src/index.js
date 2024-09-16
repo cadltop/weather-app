3d1783743f26332980926951935ff97aa0047c82
cadltop <caesarfromthecross.808@gmail.com>
2024-09-15 20:16:46 -0500

    Fix  details and refact css

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import { formInputs, tableDataCells } from "./modules/pageElements";
 formInputs.search.addEventListener("click", (event) => {
   dataFetcher(formInputs.location.value).then(function (weather) {
     tableDataCells.address.innerHTML = weather.address;
-    tableDataCells.timezone.innerHTML = weather.timezone;
+    tableDataCells.timezone.innerHTML = weather.timezone.replace("_", " ");
     tableDataCells.conditions.innerHTML = weather.conditions;
     tableDataCells.time.innerHTML = weather.time;
-    tableDataCells.temperature.innerHTML = weather.temperature;
+    tableDataCells.temperature.innerHTML = `${weather.temperature} °F`;
+
+    document.querySelector("table").style.display = "table";
   });
   event.preventDefault();
 });
